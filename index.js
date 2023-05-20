4ca5ffeeb37d3eeb45dba0845bb054e8f7dc7ab6
Jose Raphael Mercado <98337482+raphmercado@users.noreply.github.com>
2023-05-20 21:19:01 +0800

    Apply eventlistener to nums1 and operators vars

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function operate(num1, num2, operator) {
 
 }
 
-const num1 = 5;
-const num2 = 5;
-const operator = "+";
+const nums1 = document.querySelectorAll(".num");
+nums1.forEach(num => num.addEventListener("click", e => console.log(e.target.value)));
+
+const nums2 = 5;
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => operator.addEventListener("click", e => console.log(e.target.value)));
