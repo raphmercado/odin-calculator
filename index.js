0f83581b101726c8c87c7720119d3ebab93443f1
Jose Raphael Mercado <98337482+raphmercado@users.noreply.github.com>
2023-05-22 17:15:50 +0800

    Create logic that enables disabled buttons

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function operate(num1, num2, operator) {
 function getFirstNum(e) {
   firstNumArray.push(e.target.value);
   const finalNum = parseInt(firstNumArray.join(""));
+  operators.forEach(operator => enable(operator));
+  enable(clear);
   console.log(finalNum);
   console.log(isClicked);
 }
@@ -28,6 +30,7 @@ function getFirstNum(e) {
 function getSecondNum(e) {
   secondNumArray.push(e.target.value);
   const finalNum = parseInt(secondNumArray.join(""));
+  enable(equals);
   console.log(finalNum);
   console.log(isClicked);
 }
@@ -37,7 +40,7 @@ function getOperator(e) {
   isClicked = true;
   if (isClicked === true) {
     firstNum.forEach(num => num.removeEventListener("click", getFirstNum));
-    console.log("Stopped");
+    console.log("Stopped. Next: Second num");
     operators.forEach(operator => operator.removeEventListener("click", getOperator));
     isClicked = false;
     // EventListener for second number
@@ -45,9 +48,15 @@ function getOperator(e) {
   }
 }
 
+function enable(element) {
+  element.disabled = false;
+}
+
 const firstNum = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".operator");
 const secondNum = document.querySelectorAll(".num");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
 let firstNumArray = [];
 let secondNumArray = [];
 let isClicked = false;
